4eff27881ff9ec49d743b5c6d25a802c6bbd4f89
23arun <129366642+23arun@users.noreply.github.com>
2025-03-09 02:12:49 +0530

    Add files via upload

---
+++ Insurance-Project/Insurance/src/main/java/com/example/insurance/Repository/InsuranceRepository.java
@@ -0,0 +1,10 @@
+package com.example.insurance.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.insurance.Entity.InsuEntity;
+
+@Repository
+public interface InsuranceRepository extends JpaRepository<InsuEntity, Long> {
+    // Additional custom query methods can be added here
+}
